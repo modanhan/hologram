4e9d0d65383159ad51038872420dee6b27b6d8cd
Modan Han <modanhan@live.com>
2018-04-04 23:45:00 -0600

    worked on the rest of the scenes

---
+++ Assets/3ferris/FerrisRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FerrisRotate : MonoBehaviour {
+
+    public float minX, maxX, speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var r = transform.eulerAngles;
+        r.y = Mathf.Lerp(minX, maxX, (Mathf.Cos(Time.time * speed) + 1) * 0.5f);
+        transform.eulerAngles = r;
+    }
+}
---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public int currentScene, maxScene;
+
+    public GameObject scenes;
+    public Image cover;
+    public AnimationCurve curve;
+    float t;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            t = 0;
+            StartCoroutine(Switch());
+        }
+        cover.color = Color.Lerp(Color.clear, Color.black, curve.Evaluate(t));
+        t += Time.deltaTime;
+    }
+
+    IEnumerator Switch()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < scenes.transform.childCount; i++)
+        {
+            scenes.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        currentScene++;
+        currentScene %= maxScene;
+        scenes.transform.GetChild(currentScene).gameObject.SetActive(true);
+    }
+}
